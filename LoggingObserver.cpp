4e637e3aa5f10ffda1d7d23994f997a2c0e422f5
Diana <edvi.diana@gmail.com>
2025-11-06 21:22:17 -0500

    feat: added in assignment operator, stream insetion operators, and copy constructors to all classes in LoggingObserver.cpp

--- LoggingObserver.cpp
+++ LoggingObserver.cpp
@@ -1,22 +1,75 @@
-#include <iostream>
-#include <string>
-#include <fstream>
 #include "LoggingObserver.h"
+#include <iostream>
+#include <fstream>
 
-// ============= ILoggable class (interface) ===============
+// ================= ILoggable =================
 
-ILoggable::ILoggable(){}
-ILoggable::~ILoggable(){}
+ILoggable::ILoggable() {}
+ILoggable::~ILoggable() {}
 
-// ============= Observer class (abstract) ===============
+// Copy constructor
+ILoggable::ILoggable(const ILoggable&) {}
 
-Observer::Observer(){}
-Observer::~Observer(){}
+// Assignment operator
+ILoggable& ILoggable::operator=(const ILoggable&) {
+    return *this;
+}
 
-// ============= Subject class ===============
+// Stream insertion operator
+std::ostream& operator<<(std::ostream& os, ILoggable& loggable) {
+    os << loggable.stringToLog();
+    return os;
+}
 
-Subject::Subject(){}
-Subject::~Subject(){}
+// ================= Observer =================
+
+Observer::Observer() {}
+Observer::~Observer() {}
+
+// Copy constructor
+Observer::Observer(const Observer&) {}
+
+// Assignment operator
+Observer& Observer::operator=(const Observer&) {
+    return *this;
+}
+
+// Stream insertion operator
+std::ostream& operator<<(std::ostream& os, Observer& obs) {
+    os << "Observer";
+    return os;
+}
+
+// ================= Subject =================
+
+Subject::Subject() {}
+Subject::~Subject() {}
+
+// Copy constructor
+Subject::Subject(const Subject& other) {
+    // Deep copy of observers list
+    observers.clear();
+    for (auto& obs : other.observers) {
+        observers.push_back(obs); // shared_ptr handles copying automatically
+    }
+}
+
+// Assignment operator
+Subject& Subject::operator=(const Subject& other) {
+    if (this != &other) {
+        observers.clear();
+        for (auto& obs : other.observers) {
+            observers.push_back(obs); // shared_ptr copy
+        }
+    }
+    return *this;
+}
+
+// Stream insertion operator
+std::ostream& operator<<(std::ostream& os, Subject& subj) {
+    os << "Subject with " << subj.observers.size() << " observers";
+    return os;
+}
 
 void Subject::Attach(std::shared_ptr<Observer> obs){
     observers.push_back(obs);
@@ -31,22 +84,35 @@ void Subject::Notify(ILoggable* loggable){
             obs->Update(loggable);
         }
 }
-    
-// ============= LogObserver class ===============
 
-LogObserver::LogObserver(){}
-LogObserver::~LogObserver(){}
+// ================= LogObserver =================
 
-void LogObserver::Update(ILoggable* loggable){
+LogObserver::LogObserver() {}
+LogObserver::~LogObserver() {}
 
+// Copy constructor
+LogObserver::LogObserver(const LogObserver& other) : Observer(other) {}
+
+// Assignment operator
+LogObserver& LogObserver::operator=(const LogObserver& other) {
+    if (this != &other) {
+        Observer::operator=(other);
+    }
+    return *this;
+}
+
+// Stream insertion operator
+std::ostream& operator<<(std::ostream& os, LogObserver& lo) {
+    os << "LogObserver";
+    return os;
+}
+
+void LogObserver::Update(ILoggable* loggable) {
     std::ofstream file("gamelog.txt", std::ios::app);
-
-    if (file.is_open()){
+    if (file.is_open()) {
         file << loggable->stringToLog() << std::endl;
         file.close();
     }
 }
 
-int main(){
-
-}
+int main(){}
